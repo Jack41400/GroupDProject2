9bb6041b73b2c7f3d1a66a90eb06c1fcc79ec8bf
Jack P - Class <jphillips142@student.ccc.edu>
2025-10-20 13:39:43 -0500

    Finished python file

--- conversionProject.py
+++ conversionProject.py
@@ -7,5 +7,10 @@ weldezgi fisshaye
 
 This is a program converting Candian dollars to U.S dollars 
 """
-CAN= float(input(" Enter your amount in CAN $ "))
+CAN = float(input(" Enter your amount in CAN $ "))
+CANtoUSD = 0.74 # Live rate is actually $0.72
+CANtoUSDConversion = CAN * CANtoUSD
+roundedBalance = round(CANtoUSDConversion, 2)
 
+finalBalance = "$" + str(roundedBalance)
+print("You balance in USD is", finalBalance)
