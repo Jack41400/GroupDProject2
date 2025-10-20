b95a1aee06face4854531e597b82f29d8ed35136
morgankem40 <morgankem40@gmail.com>
2025-10-20 13:24:19 -0500

    Update conversionProject.py

--- conversionProject.py
+++ conversionProject.py
@@ -1,4 +1,11 @@
 """
 Group D Members
+Jack Phillips
+Sewon Ra
+Kara Morgan
+weldezgi fisshaye
 
 This is a program converting Candian dollars to U.S dollars 
+"""
+CAN= float(input(" Enter your amount in CAN $ "))
+
